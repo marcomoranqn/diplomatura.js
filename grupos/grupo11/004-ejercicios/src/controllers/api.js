a598f99c5152f182ce73129ed5cc3cddb043ce91
Gabriel Raby <grabyc@gmail.com>
2020-07-12 21:57:27 -0300

    Ej004.002 OK

--- grupos/grupo11/004-ejercicios/src/controllers/api.js
+++ grupos/grupo11/004-ejercicios/src/controllers/api.js
@@ -30,9 +30,8 @@ export const getPosts = async function(){
 };
 
 export const getUserById = async function( userId ){
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const users = await response.json();
-    const user =await  users.find( user => user.id === userId);
+    const users = await getUsers();
+    const user = users.find( (u) => u.id === userId);
 
     return user;
 };
@@ -47,97 +46,68 @@ export const getUsers = async function(){
 
 // getters para ejercicios (transforman los registros originales)
 export const getAlbumById = async function( albumId ){
+    const photos = await getPhotos();
     const response = await fetch(`https://jsonplaceholder.typicode.com/albums/${albumId}`);
-    const album = await response.json();
+    let album = await response.json();
     
-    const photos = getPhotos();
-    const newAlbum = photos
-        .then( (result) =>  ({
-                  userId: album.userId,
-                  id: album.id,
-                  title: album.title,
-                  photos: result.filter( (p) => p.albumId === album.id )
-                                  .map(
-                                    (p) =>{
-                                        return {
-                                          id: p.id,
-                                          title: p.title,
-                                          url: p.url,
-                                          thumbnailUrl: p.thumbnailUrl,
-                                        }
-                                      }
-                                  ),
-                })
-              );
+    album.photos= photos.filter( (p) => p.albumId === album.id )
+                        .map( (p) =>{
+                            return {
+                                id: p.id,
+                                title: p.title,
+                                url: p.url,
+                                thumbnailUrl: p.thumbnailUrl,
+                            }}
+                        );
 
-    return newAlbum;
+    return album;
 };
 
 export const getAlbumsFull = async function(){
+    const users = await getUsers();
     const response = await fetch("https://jsonplaceholder.typicode.com/albums");
     const albums = await response.json();
 
-    const users = getUsers();
-    const newAlbums = users
-        .then( (result) =>  
-            albums.map( (a) =>{
+    return albums.map( (a) =>{
                 return {
                   id: a.id,
                   title: a.title,
-                  user: result.find( (u) => u.id=== a.userId ),
+                  user: users.find( (u) => u.id=== a.userId ),
                 }
-              })            
-        );
-
-    return newAlbums;
+              });
 };
 
 export const getPostById = async function( postId ){
+    const comments = await getComments();
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
-    const post = await response.json();
+    let post = await response.json();
     
-    const comments = getComments();
-    const newPost = comments
-        .then( (result) =>  ({
-                  userId: post.userId,
-                  id: post.id,
-                  title: post.title,
-                  body: post.body,
-                  comments: result.filter( (c) => c.postId === post.id )
-                                  .map(
-                                    (c) =>{
-                                        return {
-                                          id: c.id,
-                                          name: c.name,
-                                          email: c.email,
-                                          body: c.body,
+    post.comments = comments.filter( (c) => c.postId === post.id )
+                            .map( (c) =>{
+                                    return {
+                                        id: c.id,
+                                        name: c.name,
+                                        email: c.email,
+                                        body: c.body,
                                         }
-                                      }
-                                  ),
-                })
-              );
+                                    });
 
-    return newPost;
+    return post;
 };
 
 export const getPostsFull = async function(){
+    const users = await getUsers();
     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
     const posts = await response.json();
 
-    const users = getUsers();
-    const newPosts = users
-        .then( (result) =>  
-            posts.map( (p) =>{
+    return posts.map( (p) =>{
                 return {
                   id: p.id,
                   title: p.title,
                   body: p.body,
-                  user: result.find( (u) => u.id=== p.userId ),
+                  user: users.find( (u) => u.id=== p.userId ),
                 }
-              })            
-        );
-
-    return newPosts;
+              });
 };
 
 export const getUsersFull = async function(){
@@ -145,9 +115,8 @@ export const getUsersFull = async function(){
     const albums = await getAlbums().then( result => result );
     const posts = await getPosts().then( result => result );
 
-    const newUsers = users.map(
-         (u) => {
-             return {
+    return users.map( (u) => {
+            return {
                  id: u.id,
                  name: u.name,
                  username: u.username,
@@ -171,10 +140,8 @@ export const getUsersFull = async function(){
                                     body: p.body,
                                 }
                             }),
-            }
-         }
+            }}
     );
 
-    return newUsers;
 };
 
