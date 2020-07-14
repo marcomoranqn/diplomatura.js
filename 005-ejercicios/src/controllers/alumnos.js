d7cbb98cfa2469a2c169170a617dae517da76553
Unknown <davidtorchinsky@gmail.com>
2020-07-14 18:45:58 -0300

    probando alumno

--- 005-ejercicios/src/controllers/alumnos.js
+++ 005-ejercicios/src/controllers/alumnos.js
@@ -1,22 +1,27 @@
 import express from 'express';
 
+import bd from 'baseDeDatos';
+
+var alumno = bd.alumno;
+
 const router = express.Router();
 
-router.get('/', function (req, res) {
-  // Completar
-  res.json({});
+router.get('/', function(req, res) {
+    // Completar
+
+    res.json(bd.alumno.find());
 });
 
-router.get('/:id', function (req, res) {
-  // Completar
-  res.json({});
+router.get('/:id', function(req, res) {
+    // Completar
+    res.json(bd.alumno.findById(req.params.id));
 });
 
-router.post('/', function (req, res) {
-  // TIP: En req.body viene los datos
+router.post('/', function(req, res) {
+    // TIP: En req.body viene los datos
 
-  // Completar
-  res.json({});
+    // Completar
+    res.json({});
 });
 
 // Completar el resto de los métodos
